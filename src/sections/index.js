65229e7ba930f88a8d383b9934f74ebad6480428
Khalid Ansari <khaliljpc@gmail.com>
2023-11-24 19:26:13 +0530

    Files added

---
+++ src/sections/index.js
@@ -0,0 +1,19 @@
+import CustomerReviews from './CustomerReviews';
+import Footer from './Footer';
+import Hero from './Hero';
+import PopularProducts from './PopularProducts';
+import Servies from './Services';
+import Subscribe from './Subscribe';
+import SuperQuality from './SuperQuality';
+import SpecialOffer from './SpecialOffer';
+
+export {
+  CustomerReviews,
+  Footer,
+  Hero,
+  PopularProducts,
+  Servies,
+  Subscribe,
+  SuperQuality,
+  SpecialOffer,
+};
